7d8f9d4f36c8f7c24261ceae69cf20bb74adfd07
Pranab Bora <prnbbora4@gmail.com>
2021-08-02 11:53:38 +0530

    deploy3

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(require('./router/auth'));
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('./client/build'));
+    app.use(express.static('./Client/build'));
     const path = require("path");
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
         console.log(__dirname);
 
     })
